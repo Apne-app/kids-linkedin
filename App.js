80ffce33a629863ece10462296fac8da24b920bd
Bhargavamacha <mbsai29@gmail.com>
2020-09-16 09:05:31 +0530

    removed comment

--- App.js
+++ App.js
@@ -53,9 +53,9 @@ const App = () => {
     <NavigationContainer ref={containerRef}>
       <Stack.Navigator>
       <Stack.Screen options={{ headerShown: false }} name="Home" component={Drawer} />
-      <Stack.Screen options={{ headerShown: false }} name="SinglePost" component={SinglePostScreen} />
-      <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
       <Stack.Screen options={{ headerShown: false }} name="Child" component={ChildScreen} />
+      <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+      <Stack.Screen options={{ headerShown: false }} name="SinglePost" component={SinglePostScreen} />
       <Stack.Screen options={{ headerShown: false }} name="Intro" component={IntroScreen} />
       <Stack.Screen options={{ headerShown: false }} name="Preview" component={ImagePreview} />
       </Stack.Navigator>
