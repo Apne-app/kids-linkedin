2795a468953652a4bbebee77ccc7345353aea85a
Bhargavamacha <mbsai29@gmail.com>
2020-11-13 15:30:09 +0530

    ui changhes

--- screens/Settings.js
+++ screens/Settings.js
@@ -7,6 +7,7 @@ import { Container, Header, Content, Form, Item, Input, Label, H1, H2, H3, Icon,
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 import { useFocusEffect } from "@react-navigation/native";
+import CompHeader from '../Modules/CompHeader'
 var height = Dimensions.get('screen').height;
 var width = Dimensions.get('screen').width;
 const Settings = ({ navigation }) => {
@@ -20,12 +21,7 @@ const Settings = ({ navigation }) => {
     }
     return (
         <ScrollView>
-            <Header noShadow style={{ backgroundColor: '#fff', height: 60, borderBottomWidth: 0, marginTop: 10 }}>
-                <Body style={{ alignItems: 'center', flexDirection: 'row' }}>
-                    <Icon type="Feather" name="arrow-left" onPress={() => navigation.navigate('Profile')} />
-                    <Title style={{ fontFamily: 'NunitoSans-Regular', fontSize: 28, marginTop: -5, marginLeft: 20, color: 'black' }}>Settings</Title>
-                </Body>
-            </Header>
+            <CompHeader screen={'Settings'} goback={()=>navigation.navigate('Profile')} icon={'back'}/>
             <View style={{ margin: 25 }}>
                 <Text style={{ fontSize: 16, fontFamily: "NunitoSans-SemiBold" }}>Kid's Name</Text>
                 <TextInput placeholder="Kid's Name" style={{ height: 55, backgroundColor: 'white', borderRadius: 27.5, marginTop: 15, color: 'black', fontFamily: 'NunitoSans-Regular', paddingHorizontal: 20 }} />
@@ -35,12 +31,12 @@ const Settings = ({ navigation }) => {
                     <Text style={{ fontSize: 16, fontFamily: "NunitoSans-Bold", marginVertical: 15, marginLeft: 23 }}>Push Notifications</Text>
                     <Right style={{ marginRight: 40 }}><Switch value={isSwitchOn} onValueChange={onToggleSwitch} color={'#327FEB'} /></Right>
                 </View>
-                <View style={{ flexDirection: 'column', marginTop:'40%' }}>
+                <View style={{ flexDirection: 'column', marginTop: '40%' }}>
                     <Button block rounded iconLeft style={{ marginTop: 20, flex: 1, borderColor: '#327FEB', backgroundColor: '#327FEB', borderWidth: 1, borderRadius: 25, height: 57, }} onPress={() => navigation.navigate('Home', {})} >
                         <Text style={{ color: "white", fontFamily: 'NunitoSans-Bold', fontSize: 17 }}>Give Feedback</Text>
                     </Button>
                     <Button block rounded style={{ marginTop: 20, flex: 1, borderColor: '#327FEB', backgroundColor: '#327FEB', borderWidth: 1, borderRadius: 25, height: 57 }} onPress={() => navigation.navigate('Home', {})} >
-                        <Text style={{ color: "white", fontFamily: 'NunitoSans-Bold', fontSize: 17, alignSelf: 'center',  marginLeft: 40 }}>Contact Us</Text>
+                        <Text style={{ color: "white", fontFamily: 'NunitoSans-Bold', fontSize: 17, alignSelf: 'center', marginLeft: 40 }}>Contact Us</Text>
                         <Icon name="whatsapp" type="Fontisto" style={{ fontSize: 20, color: '#4FCE5D' }} />
                     </Button>
                     <Button block rounded iconLeft style={{ marginTop: 20, flex: 1, borderColor: 'white', backgroundColor: 'white', borderWidth: 1, borderRadius: 25, height: 57, }} onPress={() => logout()} >
